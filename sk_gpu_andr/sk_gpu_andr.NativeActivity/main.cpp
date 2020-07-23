a21b70afdc1593e8cb7b25a951a350adbef45100
Nick Klingensmith <programmerpichu@gmail.com>
2020-07-23 15:20:26 -0700

    -Added an Android build target project

---
+++ sk_gpu_andr/sk_gpu_andr.NativeActivity/main.cpp
@@ -0,0 +1,194 @@
+#include "../../src/sk_gpu_dev.h"
+
+#include <malloc.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "AndroidProject1.NativeActivity", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "AndroidProject1.NativeActivity", __VA_ARGS__))
+
+struct saved_state {
+	float angle;
+	int32_t x;
+	int32_t y;
+};
+struct engine {
+	struct android_app* app;
+
+	ASensorManager* sensorManager;
+	const ASensor* accelerometerSensor;
+	ASensorEventQueue* sensorEventQueue;
+
+	skr_swapchain_t swapchain;
+	int animating;
+	struct saved_state state;
+};
+
+static int engine_init_display(struct engine* engine) {
+	int result = skr_init("skr_gpu.h", engine->app->window, nullptr);
+	if (!result)
+	return result;
+
+	engine->swapchain = skr_swapchain_create(skr_tex_fmt_rgba32_linear, skr_tex_fmt_depth32, 1280, 720);
+	
+	return 0;
+}
+
+/**
+* Just the current frame in the display.
+*/
+static void engine_draw_frame(struct engine* engine) {
+	skr_draw_begin();
+	float clear_color[4] = { 0,1,0,1 };
+	const skr_tex_t *target, *depth;
+	skr_swapchain_get_next(&engine->swapchain, &target, &depth);
+	skr_set_render_target(clear_color, target, depth);
+
+	skr_swapchain_present(&engine->swapchain);
+}
+
+/**
+* Tear down the EGL context currently associated with the display.
+*/
+static void engine_term_display(struct engine* engine) {
+	//skr_shutdown();
+}
+
+/**
+* Process the next input event.
+*/
+static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {
+	struct engine* engine = (struct engine*)app->userData;
+	if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
+		engine->state.x = AMotionEvent_getX(event, 0);
+		engine->state.y = AMotionEvent_getY(event, 0);
+		return 1;
+	}
+	return 0;
+}
+
+/**
+* Process the next main command.
+*/
+static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
+	struct engine* engine = (struct engine*)app->userData;
+	switch (cmd) {
+	case APP_CMD_SAVE_STATE:
+		// The system has asked us to save our current state.  Do so.
+		engine->app->savedState = malloc(sizeof(struct saved_state));
+		*((struct saved_state*)engine->app->savedState) = engine->state;
+		engine->app->savedStateSize = sizeof(struct saved_state);
+		break;
+	case APP_CMD_INIT_WINDOW:
+		// The window is being shown, get it ready.
+		if (engine->app->window != NULL) {
+			engine_init_display(engine);
+			engine_draw_frame(engine);
+		}
+		break;
+	case APP_CMD_TERM_WINDOW:
+		// The window is being hidden or closed, clean it up.
+		engine_term_display(engine);
+		break;
+	case APP_CMD_GAINED_FOCUS:
+		// When our app gains focus, we start monitoring the accelerometer.
+		if (engine->accelerometerSensor != NULL) {
+			ASensorEventQueue_enableSensor(engine->sensorEventQueue,
+				engine->accelerometerSensor);
+			// We'd like to get 60 events per second (in microseconds).
+			ASensorEventQueue_setEventRate(engine->sensorEventQueue,
+				engine->accelerometerSensor, (1000L / 60) * 1000);
+		}
+		break;
+	case APP_CMD_LOST_FOCUS:
+		// When our app loses focus, we stop monitoring the accelerometer.
+		// This is to avoid consuming battery while not being used.
+		if (engine->accelerometerSensor != NULL) {
+			ASensorEventQueue_disableSensor(engine->sensorEventQueue,
+				engine->accelerometerSensor);
+		}
+		// Also stop animating.
+		engine->animating = 0;
+		engine_draw_frame(engine);
+		break;
+	}
+}
+
+/**
+* This is the main entry point of a native application that is using
+* android_native_app_glue.  It runs in its own thread, with its own
+* event loop for receiving input events and doing other things.
+*/
+void android_main(struct android_app* state) {
+	struct engine engine;
+
+	memset(&engine, 0, sizeof(engine));
+	state->userData = &engine;
+	state->onAppCmd = engine_handle_cmd;
+	state->onInputEvent = engine_handle_input;
+	engine.app = state;
+
+	// Prepare to monitor accelerometer
+	engine.sensorManager = ASensorManager_getInstance();
+	engine.accelerometerSensor = ASensorManager_getDefaultSensor(engine.sensorManager,
+		ASENSOR_TYPE_ACCELEROMETER);
+	engine.sensorEventQueue = ASensorManager_createEventQueue(engine.sensorManager,
+		state->looper, LOOPER_ID_USER, NULL, NULL);
+
+	if (state->savedState != NULL) {
+		// We are starting with a previous saved state; restore from it.
+		engine.state = *(struct saved_state*)state->savedState;
+	}
+
+	engine.animating = 1;
+
+	// loop waiting for stuff to do.
+
+	while (1) {
+		// Read all pending events.
+		int ident;
+		int events;
+		struct android_poll_source* source;
+
+		// If not animating, we will block forever waiting for events.
+		// If animating, we loop until all events are read, then continue
+		// to draw the next frame of animation.
+		while ((ident = ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
+			(void**)&source)) >= 0) {
+
+			// Process this event.
+			if (source != NULL) {
+				source->process(state, source);
+			}
+
+			// If a sensor has data, process it now.
+			if (ident == LOOPER_ID_USER) {
+				if (engine.accelerometerSensor != NULL) {
+					ASensorEvent event;
+					while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
+						&event, 1) > 0) {
+						LOGI("accelerometer: x=%f y=%f z=%f",
+							event.acceleration.x, event.acceleration.y,
+							event.acceleration.z);
+					}
+				}
+			}
+
+			// Check if we are exiting.
+			if (state->destroyRequested != 0) {
+				engine_term_display(&engine);
+				return;
+			}
+		}
+
+		if (engine.animating) {
+			// Done with events; draw next animation frame.
+			engine.state.angle += .01f;
+			if (engine.state.angle > 1) {
+				engine.state.angle = 0;
+			}
+
+			// Drawing is throttled to the screen update rate, so there
+			// is no need to do timing here.
+			engine_draw_frame(&engine);
+		}
+	}
+}

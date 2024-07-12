813650644b275cceae186ea26bc35ca513e9cb57
Nick Klingensmith <quic_nklingen@quicinc.com>
2024-07-12 15:46:07 -0700

    Build a distribution folder locally, pairs well with StereoKit's add_directory line on Windows x64
    Signed-off-by: Nick Klingensmith <quic_nklingen@quicinc.com>

--- build.bat
+++ build.bat
@@ -1,21 +1,11 @@
-mkdir build_linux
-cd build_linux
-wsl cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel
-wsl cmake --build . -j24 --config MinSizeRel
-cd ..
+cmake --preset skshaderc_Win32_x64_Release
+cmake --build --preset skshaderc_Win32_x64_Release
 
-mkdir build_win
-cd build_win
-cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel
-cmake --build . -j24 --config MinSizeRel
-cd ..
+mkdir bin\distribute
+mkdir bin\distribute\tools
+mkdir bin\distribute\tools\win32_x64
+mkdir bin\distribute\src
 
-mkdir distribute
-mkdir distribute\example
-copy sk_gpu.h distribute\sk_gpu.h
-copy build_win\skshaderc\MinSizeRel\skshaderc.exe distribute\skshaderc.exe
-copy build_linux\skshaderc\skshaderc distribute\skshaderc
-
-copy build_win\MinSizeRel\skg_flatscreen.exe distribute\example\skg_flatscreen.exe
-copy build_linux\skg_flatscreen distribute\example\skg_flatscreen
-copy examples\platform.ply distribute\example\platform.ply
+copy bin\intermediate\Win32_x64\Release\skshaderc_exe.exe bin\distribute\tools\win32_x64\skshaderc.exe
+copy sk_gpu.h bin\distribute\src\sk_gpu.h
+copy skshaderc\CMakeLists.distribute.txt bin\distribute\CMakeLists.txt

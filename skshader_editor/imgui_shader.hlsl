03c5fe20fe9d9d028781424ec35d94e86fc0ebc7
Kurtis <keveleig@qti.qualcomm.com>
2025-05-22 20:29:08 -0700

    Tabify
    Signed-off-by: Kurtis <keveleig@qti.qualcomm.com>

--- skshader_editor/imgui_shader.hlsl
+++ skshader_editor/imgui_shader.hlsl
@@ -1,5 +1,5 @@
 cbuffer vertexBuffer : register(b0) {
-    float4x4 ProjectionMatrix;
+	float4x4 ProjectionMatrix;
 };
 
 struct vsIn {
@@ -9,9 +9,9 @@ struct vsIn {
 	float4 col  : COLOR0;
 };
 struct psIn {
-    float4 pos : SV_POSITION;
-    float4 col : COLOR0;
-    float2 uv  : TEXCOORD0;
+	float4 pos : SV_POSITION;
+	float4 col : COLOR0;
+	float2 uv  : TEXCOORD0;
 };
 
 sampler   sampler0 : register(s0);
@@ -22,13 +22,13 @@ float3 srgb_to_linear(float3 srgb) {
 }
 
 psIn vs(vsIn input) {
-    psIn output;
-    output.pos = mul( ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
-    output.col = float4(srgb_to_linear(input.col.rgb),1);
-    output.uv  = input.uv;
-    return output;
+	psIn output;
+	output.pos = mul( ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
+	output.col = float4(srgb_to_linear(input.col.rgb),1);
+	output.uv  = input.uv;
+	return output;
 }
 
 float4 ps(psIn input) : SV_Target {
-    return input.col * texture0.Sample(sampler0, input.uv);
+	return input.col * texture0.Sample(sampler0, input.uv);
 }
